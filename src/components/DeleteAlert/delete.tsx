08a52dcf20f2bc899590cfcae78831a7f7b2ed41
Jane Ahone <jane.eloundou@gmail.com>
2024-09-19 14:02:52 +0100

    Correct deployment error

--- src/components/DeleteAlert/delete.tsx
+++ src/components/DeleteAlert/delete.tsx
@@ -1,51 +1,51 @@
-import {
-  useDisclosure,
-  Button,
-  AlertDialog,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogBody,
-  AlertDialogFooter,
-} from "@chakra-ui/react";
-import React from "react";
+// import {
+//   useDisclosure,
+//   Button,
+//   AlertDialog,
+//   AlertDialogOverlay,
+//   AlertDialogContent,
+//   AlertDialogHeader,
+//   AlertDialogBody,
+//   AlertDialogFooter,
+// } from "@chakra-ui/react";
+// import React from "react";
 
-function AlertDialogExample() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const cancelRef = React.useRef();
+// function AlertDialogExample() {
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   const cancelRef = React.useRef();
 
-  return (
-    <>
-      <Button colorScheme="red" onClick={onOpen}>
-        Delete Customer
-      </Button>
+//   return (
+//     <>
+//       <Button colorScheme="red" onClick={onOpen}>
+//         Delete Customer
+//       </Button>
 
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay>
-          <AlertDialogContent>
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              Delete Customer
-            </AlertDialogHeader>
+//       <AlertDialog
+//         isOpen={isOpen}
+//         leastDestructiveRef={cancelRef}
+//         onClose={onClose}
+//       >
+//         <AlertDialogOverlay>
+//           <AlertDialogContent>
+//             <AlertDialogHeader fontSize="lg" fontWeight="bold">
+//               Delete Customer
+//             </AlertDialogHeader>
 
-            <AlertDialogBody>
-              Are you sure? You can&#39t undo this action afterwards.
-            </AlertDialogBody>
+//             <AlertDialogBody>
+//               Are you sure? You can&#39t undo this action afterwards.
+//             </AlertDialogBody>
 
-            <AlertDialogFooter>
-              <Button ref={cancelRef} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme="red" onClick={onClose} ml={3}>
-                Delete
-              </Button>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialogOverlay>
-      </AlertDialog>
-    </>
-  );
-}
+//             <AlertDialogFooter>
+//               <Button ref={cancelRef} onClick={onClose}>
+//                 Cancel
+//               </Button>
+//               <Button colorScheme="red" onClick={onClose} ml={3}>
+//                 Delete
+//               </Button>
+//             </AlertDialogFooter>
+//           </AlertDialogContent>
+//         </AlertDialogOverlay>
+//       </AlertDialog>
+//     </>
+//   );
+// }
